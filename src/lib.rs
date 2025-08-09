7780cd7f83c85abb07a7fc0163e18362916d137b
h3athen <gurungaryan73@gmail.com>
2025-08-09 18:32:27 +0530

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use windows::{ Win32::Foundation::*, Win32::System::SystemServices::*, };
+
+#[unsafe(no_mangle)]
+#[allow(non_snake_case, unused_variables)]
+extern "system" fn DllMain(
+    dll_module: HINSTANCE,
+    call_reason: u32,
+    _: *mut ())
+    -> bool
+{
+    match call_reason {
+        DLL_PROCESS_ATTACH => (),
+        DLL_PROCESS_DETACH => (),
+        _ => ()
+    }
+
+    true
+}

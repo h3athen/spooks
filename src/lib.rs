1ff1dcea603bcfd0450cbd6c6441266a780d0a45
h3athen <gurungaryan73@gmail.com>
2025-09-03 11:40:52 +0530

    Implement DllMain with initialization and cleanup logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
+use std::sync::Once;
+use std::fmt::Result;
 use windows::{ Win32::Foundation::*, Win32::System::SystemServices::*, };
+
+
+static INIT: Once = Once::new();
 
 #[unsafe(no_mangle)]
 #[allow(non_snake_case, unused_variables)]
@@ -9,10 +14,30 @@ extern "system" fn DllMain(
     -> bool
 {
     match call_reason {
-        DLL_PROCESS_ATTACH => (),
-        DLL_PROCESS_DETACH => (),
+        DLL_PROCESS_ATTACH => {
+            INIT.call_once(|| {
+                if let Err(e) = initialize() {
+                    eprintln!("Failed to initialize: {:?}", e);
+                }
+            })
+        },
+        DLL_PROCESS_DETACH => {
+            INIT.call_once(|| {
+                if let Err(e) = cleanup() {
+                    eprintln!("Failed to cleanup: {:?}", e);
+                }
+            })
+        },
         _ => ()
     }
 
     true
+}
+
+fn initialize() -> Result {
+    Ok(())
+}
+
+fn cleanup() -> Result {
+    Ok(())
 }
